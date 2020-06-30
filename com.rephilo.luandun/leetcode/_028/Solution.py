dc135b8e6f41ff98326b770d2b03034606143df5
王霄 <wangxiao15@maoyan.com>
2020-06-30 17:03:23 +0800

    028

---
+++ com.rephilo.luandun/leetcode/_028/Solution.py
@@ -0,0 +1,20 @@
+def strStr0(haystack: str, needle: str) -> int:
+    """
+    内置方法 find方法没找到会返回-1，index方法没找到会抛异常，找到的情况一致
+    :param self:
+    :param haystack:
+    :param needle:
+    :return:
+    """
+    return haystack.find(needle)
+
+
+def strStr1(haystack: str, needle: str) -> int:
+    """
+    内置方法 find方法没找到会返回-1，index方法没找到会抛异常，找到的情况一致
+    :param self:
+    :param haystack:
+    :param needle:
+    :return:
+    """
+    return haystack.find(needle)
