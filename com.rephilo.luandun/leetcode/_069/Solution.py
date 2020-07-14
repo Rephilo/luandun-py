5cb2f918c301acd5033b3c86d91caa156191bc2d
王霄 <wangxiao15@maoyan.com>
2020-07-14 20:05:22 +0800

    069

--- com.rephilo.luandun/leetcode/_069/Solution.py
+++ com.rephilo.luandun/leetcode/_069/Solution.py
@@ -1,10 +1,30 @@
 import math
 
 
-def mySqrt(x: int) -> int:
+def mySqrt1(x: int) -> int:
     """
     内置函数
     :param x:
     :return:
     """
     return int(math.sqrt(x))
+
+
+def mySqrt2(x: int) -> int:
+    """
+    二分查找法
+    :param x:
+    :return:
+    """
+    left = 0
+    right = x
+    result = -1
+    while left <= right:
+        mid = left + (right - left) // 2
+        currX = mid * mid
+        if currX <= x:
+            result = mid
+            left = mid + 1
+        elif currX > x:
+            right = mid - 1
+    return result
